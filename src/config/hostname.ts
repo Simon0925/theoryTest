05ad12ec3b4715b60c59ff86d8e85e4bcfbe1458
Sam <49652580+Simon0925@users.noreply.github.com>
2024-11-13 10:39:43 +0000

    vercel test

--- src/config/hostname.ts
+++ src/config/hostname.ts
@@ -1,6 +1,6 @@
 // const hostname = 'http://localhost:8080';
-// const hostname = 'https://theorytestback-1.onrender.com';
-const hostname = 'https://theory-test-back.vercel.app';
+const hostname = 'https://theorytestback-1.onrender.com';
+// const hostname = 'https://theory-test-back.vercel.app';
 
 
 
